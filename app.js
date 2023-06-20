d1e017a682bcf69bc3802ed29ae6108c13e69999
Shruti-Koli <koli.shruti55@gmail.com>
2023-06-20 12:31:10 +0530

    reqAndRes

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    console.log("Shruti")
+    console.log(req)
+    if(req.url == "/home"){
+        res.write('<html>');
+        res.write('<head><title>home</title></head>')
+        res.write('<body><h1>Welcome Home</h1></body>')
+        res.write('<html>');
+    }else if( req.url=="/about"){
+        res.write('<html>');
+        res.write('<head><title>about</title></head>')
+        res.write('<body><h1>Welcome to about</h1></body>')
+        res.write('<html>');
+    }
+    else if(req.url=="/node" ){
+        res.write('<html>');
+        res.write('<head><title>node</title></head>')
+        res.write('<body><h1>Welcome to node</h1></body>')
+        res.write('<html>');
+    }else{
+        res.write('<html>');
+        res.write('<head><title>error</title></head>')
+        res.write('<body><h1>Wrong URL</h1></body>')
+        res.write('<html>');
+    }
 });
 
-server.listen(4000)
+server.listen(4000);
